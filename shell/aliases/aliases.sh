931e9885f90c1df83212c3b979ea29fe55a32b3a
Ivan Vanderbyl <ivanvanderbyl@me.com>
2013-12-13 10:29:36 +1100

    Fixed the internet

--- shell/aliases/aliases.sh
+++ shell/aliases/aliases.sh
@@ -1,7 +1,9 @@
 alias ls="ls -laGF"                           # compact view, show color
+alias ll="ls -lsahGF"
 alias history="fc -l 1"                       # show more the 16 history items
 alias z="_z -r 2>&1"                          # highest rated results first
 alias internet\?="ping 8.8.8.8"               # google?
+alias google\?="ping 8.8.8.8"
 
 alias b="bundle"                              # run bundle
 alias be="bundle exec"                        # run bundle
