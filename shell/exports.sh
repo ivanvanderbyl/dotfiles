e13e7fd84203151a35d1779345b4afeeeed69f22
Ivan Vanderbyl <ivanvanderbyl@me.com>
2013-12-13 10:30:04 +1100

    Setup editor and added go path

--- shell/exports.sh
+++ shell/exports.sh
@@ -1,6 +1,10 @@
 export GOPATH=~/dev/go
 # export GOROOT=~/dev/go
 
+export EDITOR=subl
+
+export PATH="$GOPATH/bin:$PATH"
+
 # Use Homebrew's directories rather than ~/.rbenv
 # export RBENV_ROOT=/usr/local/var/rbenv
 export RBENV_ROOT=~/.rbenv
