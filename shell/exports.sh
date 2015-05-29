2db41fbd53993c737650a42d17b35637da70a9a5
Ivan Vanderbyl <ivan@flood.io>
2015-05-29 11:54:17 +1000

    Added boot2docker hosts

--- shell/exports.sh
+++ shell/exports.sh
@@ -4,7 +4,13 @@ export GOPATH=~/dev/go
 export EDITOR=subl
 
 export PATH="$GOPATH/bin:$PATH"
+export PATH="/usr/local/sbin:$PATH"
 
 # Use Homebrew's directories rather than ~/.rbenv
 # export RBENV_ROOT=/usr/local/var/rbenv
 export RBENV_ROOT=~/.rbenv
+
+
+export DOCKER_HOST=tcp://192.168.59.103:2376
+export DOCKER_CERT_PATH=/Users/ivanvanderbyl/.boot2docker/certs/boot2docker-vm
+export DOCKER_TLS_VERIFY=1
